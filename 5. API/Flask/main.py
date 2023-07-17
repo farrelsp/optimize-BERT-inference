1c6f4c5bdc3063f7bf0a5c3ab00b6e8e5dc97adf
farrelsp <m.farrelsatyaputra@gmail.com>
2023-07-17 11:23:34 +0700

    Experimenting on fastapi and flask performances

---
+++ 5. API/Flask/main.py
@@ -0,0 +1,62 @@
+from flask import Flask, request
+
+import time
+import numpy as np
+import pandas as pd
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+import torch
+from torch import optim
+import torch.nn.functional as F
+
+from transformers import BertForSequenceClassification, BertConfig, BertTokenizer
+
+from onnxruntime.quantization import quantize_dynamic, QuantType
+import onnxruntime as ort
+from onnxruntime import InferenceSession
+
+app = Flask(__name__)
+
+path = "./../../model/indobert-onnx"
+model_path = path + "/quantized_optimized.onnx"
+session = InferenceSession(model_path, providers=["CPUExecutionProvider"])
+tokenizer = BertTokenizer.from_pretrained(path)
+
+i2w = {0: 'positive', 1: 'neutral', 2: 'negative'}
+
+@app.route("/")
+def root():
+    return {"message": "Hello World"}
+
+@app.route("/sentiment/predict/", methods=['POST'])
+def predict():
+    start = time.time()
+
+    # Tokenize input
+    review = request.json['text']
+    inputs = tokenizer([review])
+    inputs_onnx = dict(
+        input_ids=np.array(inputs["input_ids"]).astype("int64"),
+        attention_mask=np.array(inputs["attention_mask"]).astype("int64"),
+        token_type_ids=np.array(inputs["token_type_ids"]).astype("int64")
+    )
+
+    # Model Inference
+    logits = session.run(None, input_feed=inputs_onnx)[0]
+    label = torch.topk(torch.from_numpy(logits), k=1, dim=-1)[1].squeeze().item()
+    probability = F.softmax(torch.from_numpy(logits), dim=-1).squeeze()[label].item()
+
+    end = time.time()
+    duration = end - start
+
+    result = {
+        "text": review,
+        "label": i2w[label],
+        "probability": probability,
+        "duration": duration
+    }
+
+    print(f"Label: {i2w[label]}")
+    return result
